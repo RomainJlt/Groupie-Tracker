d830f99dea09fb6b6fc6e3d5cef0d2cd238440ce
cleo.deroo@ynov.com <cleo.deroo@ynov.com>
2024-01-30 07:26:33 +0100

    ajout de marqueur et d'info sur le marqueur

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([47.2, -1.55], 5);
+
+var markerLondon = L.marker([51.505, -0.09]).addTo(mymap);
+markerLondon.bindPopup("<b>Londres: 30 février gazo</b>").openPopup();
+
+var markerNantes = L.marker([47.2, -1.55]).addTo(mymap);
+markerNantes.bindPopup("<b>Nantes</b>").openPopup();
+
+var markerParis = L.marker([48.8566, 2.3522]).addTo(mymap);
+markerParis.bindPopup("<b>Paris</b>").openPopup();
+
+var markerBerlin = L.marker([52.5200, 13.4050]).addTo(mymap);
+markerBerlin.bindPopup("<b>Berlin</b>").openPopup();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
