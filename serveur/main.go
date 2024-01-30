886d54d2149350abadab348e20737697a08921d1
baarthur <baudet.arthur@ynov.com>
2024-01-30 08:48:39 +0100

    ajout du js

--- serveur/main.go
+++ serveur/main.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler.Index)
-	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("../styles"))))
 	http.HandleFunc("/info_artiste", handler.InfoArtiste)
 	http.HandleFunc("/index", handler.Index)
+
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("../styles"))))
 	
 	http.ListenAndServe(":8080", nil)
 
