a330d1cea5d418837373000b166690867dc876ba
RomainJlt <romainjollivet0@gmail.com>
2024-02-20 07:49:35 +0100

    map

---
+++ js/map.js
@@ -0,0 +1,7 @@
+// Créer une carte OpenStreetMap
+var map = L.map('map').setView([47.20567535891242, -1.5393572608259531], 13); // Coordonnées initiales et niveau de zoo
+
+// Ajouter une couche de tuiles OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
