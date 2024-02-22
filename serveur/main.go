a195a5618d450ba6bbe0f0b423b9307a0c60b305
baarthur <baudet.arthur@ynov.com>
2024-02-22 10:43:38 +0100

    search bar

--- serveur/main.go
+++ serveur/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"groupie/handler"
 	"net/http"
+	"fmt"
 )
 
 func main() {
-	http.HandleFunc("/", handler.Index)
-	http.HandleFunc("/info_artiste", handler.InfoArtiste)
-	http.HandleFunc("/index", handler.Index)
+	http.HandleFunc("/", handler.ArtistHandler)
+	// ttp.HandleFunc("/info_artiste", handler.InfoArtiste)
 
 	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("../styles"))))
+
+	fmt.Println("Server is running")
 	
 	http.ListenAndServe(":8080", nil)
-
-	
 }
